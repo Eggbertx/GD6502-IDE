e36e83a74db945a4ae9c922cdd7da2e1dce42556
Eggbertx <eggbertx@users.noreply.github.com>
2024-07-25 18:06:10 -0700

    Update gdUnit4 to 4.3.4

--- addons/gdUnit4/src/ui/menu/ScriptEditorContextMenuHandler.gd
+++ addons/gdUnit4/src/ui/menu/ScriptEditorContextMenuHandler.gd
@@ -1,4 +1,4 @@
-class_name ScriptEditorContextMenuHandler
+@tool
 extends Control
 
 var _context_menus := Dictionary()
@@ -12,16 +12,6 @@ func _init(context_menus: Array[GdUnitContextMenuItem]) -> void:
 	_editor = EditorInterface.get_script_editor()
 	_editor.editor_script_changed.connect(on_script_changed)
 	on_script_changed(active_script())
-
-
-static func dispose() -> void:
-	var handler: ScriptEditorContextMenuHandler = Engine.get_main_loop().root.find_child("ScriptEditorContextMenuHandler*", false, false)
-	if handler:
-		var editor := EditorInterface.get_script_editor()
-		if editor.editor_script_changed.is_connected(handler.on_script_changed):
-			editor.editor_script_changed.disconnect(handler.on_script_changed)
-		Engine.get_main_loop().root.call_deferred("remove_child", handler)
-		handler.queue_free()
 
 
 func _input(event: InputEvent) -> void:
