6a38604bedfbc490ca8089f7b4fb7a8653eff409
Eggbertx <eggbertx@users.noreply.github.com>
2024-12-07 15:34:21 -0800

    Fix startup bottleneck (only update screen when needed)

--- src/Screen.gd
+++ src/Screen.gd
@@ -14,21 +14,28 @@ const num_pixels = 0x400
 var img := Image.create(32, 32, true, Image.FORMAT_RGB8)
 var max_queue_timer := 1.0 / 600.0
 var queue_timer := 0.0
+var needs_update := false
 
 func _ready():
 	img.fill(palette[0])
 	set_texture(ImageTexture.create_from_image(img))
+	needs_update = true
 
 func set_pixel_col(index:int, color:int):
 	var x := index % 32
 	var y := floori(index / 32.0)
 	img.set_pixel(x, y, palette[color & 0xf])
+	needs_update = true
 
 func _draw():
-	texture.update(img)
+	if needs_update:
+		texture.update(img)
+		needs_update = false
 
 func _process(_delta):
-	queue_redraw()
+	if needs_update:
+		queue_redraw()
 
 func clear():
 	img.fill(palette[0])
+	needs_update = true
