b3dd9901582d3062343650365142679f18232515
Eggbertx <eggbertx@users.noreply.github.com>
2024-12-26 16:52:20 -0800

    allow reading stored byte in $ff

--- src/ExampleCPUSubclass.gd
+++ src/ExampleCPUSubclass.gd
@@ -1,8 +1,6 @@
 class_name ExampleCPUSubclass
 extends CPU
 
-var _current_key := 0
-
 func _setup_specs():
 	memory_size = 0x5ff
 	sp_start = 0xff
@@ -12,15 +10,13 @@ func get_byte(addr: int) -> int:
 	match addr:
 		0xfe:
 			return randi_range(0, 255)
-		0xff:
-			return _current_key
 		_:
 			return super(addr)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		var ke := event as InputEventKey
-		_current_key = ke.unicode & 0xff
+		set_byte(0xff, ke.unicode & 0xff)
 
 
 func step(steps:int = 1):
